ecec6a3d8abbbf7725c50ff436c8a808327bdcf4
Jesray Garciano <jesray.garciano@gmail.com>
2018-02-06 12:52:08 +0800

    Adding login with github and facebook

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,6 +55,19 @@
                 <a href="/password/email">Forgot Your Password?</a>
               </div>
             </div>
+            <div class="col-md-6 col-md-offset-4">
+              <hr>
+              <a href="#" style="display: block;">
+                <div class="input-group" style="border: 1px solid #cecece; width: 100%; padding: 5px; border-left: 6px solid #0b5390;">
+                  <i class="fa fa-3x fa-facebook-square" style="color:#326087; vertical-align: middle;"></i> Login Using Facebook
+                </div>
+              </a>
+              <a href="#" style="display: block; color: black;">
+                <div class="input-group" style="margin-top:5px; border: 1px solid #cecece; width: 100%; padding: 5px; border-left: 6px solid #5e5e5e;">
+                  <i class="fa fa-3x fa-github-square" style="color:#000000; vertical-align: middle;"></i> Login Using Github
+                </div>
+              </a>
+            </div>
           </form>
         </div><!-- .panel-body -->
       </div><!-- .panel -->
