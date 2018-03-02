8199abc824cb6fb24cecd42ff810a3e9c908c030
Jesray Garciano <jesray.garciano@gmail.com>
2018-03-02 15:36:58 +0800

    Started management side for ITP

--- resources/views/companies/followed_list.blade.php
+++ resources/views/companies/followed_list.blade.php
@@ -11,13 +11,13 @@
 
     <div class="row" id="followed-company_lists">
         <div class="col-md-10 col-sm-9 col-xs-12">
-                @if (count($follows) > 0)
-                        @foreach ($follows as $follow)
-                                <div id="first-comp-list" class="col-xs-12 col-sm-12 col-md-6">
-                                    @include('companies.company-container',['company'=>$follow])
-                                </div>
-                        @endforeach
-                @endif
+            @if (count($follows) > 0)
+                @foreach ($follows as $follow)
+                        <div id="first-comp-list" class="col-xs-12 col-sm-12 col-md-6">
+                            @include('companies.company-container',['company'=>$follow])
+                        </div>
+                @endforeach
+            @endif
         </div>
         <div id="ad-for-followed-list" class="col-md-2 col-sm-3">
             <div class="text-center advertisement-2">
